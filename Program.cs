e6faafb4a2494d2c49da7612eeb703dfe8795c0a
Bradley Powers <bradspowers@outlook.com>
2023-06-20 09:00:31 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using Opulos.Core.UI;
+
+namespace AccordionDemo {
+static class Program {
+	[STAThread]
+	static void Main() {
+		Application.EnableVisualStyles();
+		Application.SetCompatibleTextRenderingDefault(false);
+		Application.Run(new AccordionDemoForm());
+	}
+}
+}
